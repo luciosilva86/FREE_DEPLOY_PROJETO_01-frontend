51716e4cb7fdb1fb8c0c9ca8dcc24298833cb1ed
luciosilva86 <187441590+luciosilva86@users.noreply.github.com>
2025-10-20 16:29:11 -0400

    DevOps: Corrigida estrutura de pastas, movendo arquivos de src/source para src/.

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-// Correção CRÍTICA: O nome do componente E a extensão do arquivo
-import Aplicativo from './Aplicativo.jsx'; 
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Aplicativo /> // O nome do componente (Aplicativo) deve ser usado aqui também
+    <App />
   </React.StrictMode>,
-);
+)
+
