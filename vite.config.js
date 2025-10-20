a16f381e2ce0837499fd1e6d051fecb1d86bf52d
Lúcio Rafael Ferreira da Silva <luciorafaelferreiradasilva@gmail.com>
2025-10-20 14:17:41 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Removemos 'root: "./"' porque o index.html estará na raiz.
+  // Configuração padrão de build: o Vite buscará o index.html na raiz
   build: {
     outDir: 'dist', 
     emptyOutDir: true,
